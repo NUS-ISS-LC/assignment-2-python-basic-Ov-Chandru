dd6169e4b6e4a773d988025064bda85324ada5b0
Ov-Chandru <E1554145@u.nus.edu>
2025-11-30 22:01:50 +0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,7 +1,13 @@
 def find(s, n):
 # write your implementation here
-lens=len(s)
-print(lens)
-    return None
+    lens=len(s)
+    print(lens)
+    for i in range(lens):
+        for j in range(lens-1-i):
+            if s[i] + s[j+1+i] == n:
+                a= i
+                b= j+1+i
+    return a, b
 
 s=[2,7,9,15]
+print(find(s,9))
